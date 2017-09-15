90bd830a3b8fda12eac0b5c40969bd74bddecd63
Suzanne Wu <wu.wu.wu.world@gmail.com>
2017-09-15 19:55:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,10 @@
 var name = "Joe"
 var height = 74
 var message = `${name} is ${height} inches tall`
-`` var message = `name is height inches tall` ``
+
+var message2 = '${name} is ${height} inches tall'
+
 console.log(message)
+console.log(message2)
 
 module.exports = {name, height, message}
